e2a0f3ec89e68b3ea66e47f2b9b6009fab308c53
Joshua Morency <jsmorency@hotmail.com>
2022-10-23 16:33:21 -0400

    added login and start of main menu

--- client/Splendor Client/Assets/Scripts/Parallax.cs
+++ client/Splendor Client/Assets/Scripts/Parallax.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Parallax : MonoBehaviour
-{
+public class Parallax : MonoBehaviour {
     public float depth;
     public RectTransform rt;
-    
-    void OnEnable()
-    {
+
+    void OnEnable() {
         rt = GetComponent<RectTransform>();
     }
 
-    void Update()
-    {
+    void Update() {
         Vector2 mousePos = Mouse.current.position.ReadValue();
-        Vector2 middle = new Vector2(Screen.width/2, Screen.height/2);
+        Vector2 middle = new Vector2(Screen.width / 2, Screen.height / 2);
         Vector2 distance = new Vector2((mousePos.x - middle.x) / depth, (mousePos.y - middle.y) / depth);
         rt.position = new Vector2(middle.x + distance.x, middle.y + distance.y);
     }
