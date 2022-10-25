a685c2ccc696096236cb3d6c811b025a543227be
Joshua Morency <jsmorency@hotmail.com>
2022-10-25 15:13:02 -0400

    added most main menu functionality

---
+++ client/Splendor Client/Assets/Scripts/MenuManagers/SessionSlot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SessionSlot : MonoBehaviour {
+    public Text nameBox;
+    [SerializeField] private MainMenuManager thisManager;
+    [SerializeField] private Session thisSession;
+    //for now clicking join on a session will just put user into a test session
+    public void JoinSession() {
+        //empty for now, may not even be necessary
+    }
+
+    public void Setup(MainMenuManager newManager, Session newSession) {
+        thisSession = newSession;
+        thisManager = newManager;
+        SetText();
+    }
+
+    public void SetText() {
+        nameBox.text = thisSession.sessionName;
+    }
+}
---
+++ client/Splendor Client/Assets/Scripts/ScriptableObjects/Scripts/Save.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Save : ScriptableObject {
+    //insert save file information, probably wont stay as a ScriptableObject
+    public string saveName;
+}
