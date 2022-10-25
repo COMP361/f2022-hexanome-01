eef0180e3e3ebe0ea0b46a24ccbec7a00db3a5cd
Joshua Morency <jsmorency@hotmail.com>
2022-10-25 15:13:02 -0400

    added most main menu functionality

---
+++ client/Splendor Client/Assets/Scripts/MenuManagers/SaveSlot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveSlot : MonoBehaviour {
+    public Text nameBox;
+    [SerializeField] private MainMenuManager thisManager;
+    [SerializeField] private Save thisSave;
+    //for now clicking load on a save will just put user into a session, as if they did create session
+    public void LoadSave() {
+        //empty for now, may not even be necessary
+    }
+
+    public void Setup(MainMenuManager newManager, Save newSave) {
+        thisSave = newSave;
+        thisManager = newManager;
+        SetText();
+    }
+
+    public void SetText() {
+        nameBox.text = thisSave.saveName;
+    }
+}
