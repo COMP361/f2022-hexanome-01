a5e92c72e845b654c19f8ba5360278723d7a9f10
kevinjycui <kevinjycui@protonmail.com>
2022-10-25 20:32:50 -0400

    Add comments

--- client/Splendor Client/Assets/Scripts/Game/Player.cs
+++ client/Splendor Client/Assets/Scripts/Game/Player.cs
@@ -8,11 +8,6 @@ public class Player : MonoBehaviour
 
     private int pointsTotal = 0;
 
-    public int GetPoints()
-    {
-        return pointsTotal;
-    }
-
     public void TriggerPointsAdd(Card cardObject)
     {
         pointsTotal += cardObject.GetPoints();
