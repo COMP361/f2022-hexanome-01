41661fb689af980bcde44d600627a0f880cf4725
kevinjycui <kevinjycui@protonmail.com>
2022-10-25 21:13:04 -0400

    Display purchase option on card select

--- client/Splendor Client/Assets/Scripts/Game/Dashboard.cs
+++ client/Splendor Client/Assets/Scripts/Game/Dashboard.cs
@@ -7,9 +7,20 @@ using UnityEngine.UI;
 public class Dashboard : MonoBehaviour
 {
     public Text ptsDisplay;
+    public Text endDisplay;
 
     public void UpdatePtsDisplay(int pts)
     {
         ptsDisplay.text = String.Format("Points: {0}", pts);
+    }
+
+    public void DisplayPurchase()
+    {
+        endDisplay.text = "Purchase";
+    }
+
+    public void ResetEndDisplay()
+    {
+        endDisplay.text = "End Turn";
     }
 }
--- client/Splendor Client/Assets/Scripts/Game/Player.cs
+++ client/Splendor Client/Assets/Scripts/Game/Player.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Dashboard dashboard;
-
     private int pointsTotal = 0;
+
+    public int GetPoints()
+    {
+        return pointsTotal;
+    }
 
     public void TriggerPointsAdd(Card cardObject)
     {
         pointsTotal += cardObject.GetPoints();
-        dashboard.UpdatePtsDisplay(pointsTotal);
     }
 
 }
