2f6ef2c32ec92bdf0d70c9b33092c7226823dd70
cadinsl <46573438+cadinsl@users.noreply.github.com>
2022-10-28 14:42:22 -0400

    Added player inventory, Level1 card assets and deck
    - Modified the player script now has a list of cards that represent the player's inventory.
    - Added Card Slot which will be the script that has the game functionality for a card.
    - Created card assets that have properties of each level 1 card.
    - Deck of cards script that allows you to shuffle the deck and draw a new card.
    - Everytime a card is purchased it is now replaced with a new card on the deck

--- client/Splendor Client/Assets/Scripts/Game/CardRow.cs
+++ client/Splendor Client/Assets/Scripts/Game/CardRow.cs
@@ -7,8 +7,9 @@ public class CardRow : MonoBehaviour
 {
     [SerializeField] private int size;
     [SerializeField] private int level;
+    public Deck deck;
 
-    private Card[] cards = new Card[5];
+    private CardSlot[] cards = new CardSlot[4];
 
     public float x;
     public float y;
@@ -24,7 +25,7 @@ public class CardRow : MonoBehaviour
         }
     }
 
-    public void GreyOutExcept(Card _card)
+    public void GreyOutExcept(CardSlot _card)
     {
         for (int i=0; i<size; i++) {
             if (cards[i] != _card) cards[i].GreyOut();
@@ -44,12 +45,38 @@ public class CardRow : MonoBehaviour
 
     void FillEmptyCards()
     {
+        deck.ShuffleDeck();
         for (int i=0; i<size; i++) {
             if (cards[i] == null) {
                 GameObject prefab = Instantiate(cardObject, new Vector3(x + i*2, y, 0), Quaternion.identity);
-                cards[i] = prefab.GetComponent<Card>();
+                cards[i] = prefab.GetComponent<CardSlot>();
+                cards[i].SetCard(deck.DrawCard());
             }
         }
+    }
+
+    public void RemoveCard(CardSlot cardToRemove)
+    {
+        for(int i = 0; i < cards.Length; i++)
+        {
+            if(cards[i] == cardToRemove)
+            {
+                Destroy(cards[i].gameObject);
+                fillEmptyCardSpot(i);
+            }
+        }
+    }
+
+    private void fillEmptyCardSpot(int cardIndex)
+    {
+        GameObject prefab = Instantiate(cardObject, new Vector3(x + cardIndex*2, y, 0), Quaternion.identity);
+        cards[cardIndex] = prefab.GetComponent<CardSlot>();
+        cards[cardIndex].SetCard(deck.DrawCard());
+    }
+
+    public CardSlot GetCard(int cardIndex)
+    {
+        return cards[cardIndex];
     }
 
     void Start()
--- client/Splendor Client/Assets/Scripts/Game/Player.cs
+++ client/Splendor Client/Assets/Scripts/Game/Player.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private int pointsTotal = 0;
+    //This will be a list of the ids of the purchased player cards
+    public List<Card> inventory = new List<Card>();
 
     public int GetPoints()
     {
         return pointsTotal;
     }
 
-    public void TriggerPointsAdd(Card cardObject)
+    public void TriggerCardAdd(Card cardObject)
     {
         pointsTotal += cardObject.GetPoints();
+        inventory.Add(cardObject);
     }
 
 }
