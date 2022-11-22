f343e83d43ced090f16696eb0c8dcabf69d7dc57
Jeremy1999 <jeremyeichler1999@gmail.com>
2022-11-22 15:33:48 -0500

    Nobles now appear in inventory once impressed

--- client/Splendor Client/Assets/Scripts/Game/InventoryPanel.cs
+++ client/Splendor Client/Assets/Scripts/Game/InventoryPanel.cs
@@ -10,29 +10,41 @@ public class InventoryPanel : MonoBehaviour
 {
     public Player player;
     public GameObject cardInventoryPrefab;//Blank card gameobject that we will change its image depending on player's inventory
+    public GameObject nobleInventoryPrefab;//Blank noble gameobject that we will change its image depending on player's inventory
     public Transform purchasedCardsParent;//Parent as to where we will display the cards in the inventory
+    public Transform purchasedNoblesParent;//Parent as to where we will display the nobles in the inventory
 
     //Display is called by the button to open up the panel
     public void Display()
     {
-        DisplayPlayerCards(player.inventory);
+        DisplayPlayerCards(player.inventory, player.noblesVisited);
     }
-    public void DisplayPlayerCards(List<Card> playerCards)
+    public void DisplayPlayerCards(List<Card> playerCards, List<Noble> playerNobles)
     {
-        ClearChildren(purchasedCardsParent);
+        ClearChildren(purchasedCardsParent, purchasedNoblesParent);
         foreach(Card card in playerCards)
         {
             GameObject cardInstance = Instantiate(cardInventoryPrefab, purchasedCardsParent);
             Image cardImage = cardInstance.GetComponent<Image>();
             cardImage.sprite = card.sprite;
         }
+        foreach(Noble noble in playerNobles)
+        {
+            GameObject nobleInstance = Instantiate(nobleInventoryPrefab, purchasedNoblesParent);
+            Image nobleImage = nobleInstance.GetComponent<Image>();
+            nobleImage.sprite = noble.sprite;
+        }
     }
     //Need to clear children to not repeat cards everytime we open and close the panel
-    private void ClearChildren(Transform parent)
+    private void ClearChildren(Transform parent, Transform nobleParent)
     {
         foreach(Transform child in parent)
         {
             Destroy(child.gameObject);
+        }
+        foreach(Transform child in nobleParent){
+            Destroy(child.gameObject);
+            
         }
     }
 }
--- client/Splendor Client/Assets/Scripts/Game/Noble.cs
+++ client/Splendor Client/Assets/Scripts/Game/Noble.cs
@@ -2,6 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class NobleGemValue {
+       int _red;
+      int _green;
+      int _blue;
+      int _brown;
+      int _white;
+
+      public NobleGemValue(){
+          this._red = 0;
+          this._green = 0;
+          this._blue = 0;
+          this._brown = 0;
+          this._white = 0;
+      }
+
+
+
+     public int red
+    {
+        get { return _red; }
+        set { _red = value; }
+    }
+    public int green
+    {
+        get { return _green; }
+        set { _green = value; }
+    }
+    public int blue
+    {
+        get { return _blue; }
+        set { _blue = value; }
+    }
+    public int brown
+    {
+        get { return _brown; }
+        set { _brown = value; }
+    }
+    public int white
+    {
+        get { return _white; }
+        set { _white = value; }
+    }
+}
+
+
 [CreateAssetMenu]
 public class Noble : ScriptableObject
 {
@@ -9,11 +55,14 @@ public class Noble : ScriptableObject
 
     [SerializeField] public int id;
     [SerializeField] private int points;
-    [SerializeField] public int white;
-    [SerializeField] public int brown;
-    [SerializeField] public int blue;
-    [SerializeField] public int green;
-    [SerializeField] public int red;
+    [SerializeField] private int red;
+    [SerializeField] private int blue;
+    [SerializeField] private int green;
+    [SerializeField] private int brown;
+    [SerializeField] private int white;
+
+    
+    [SerializeField] public NobleGemValue nobleValue = new NobleGemValue();
 
     private bool active = true;
 
@@ -30,9 +79,13 @@ public class Noble : ScriptableObject
 
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
+        nobleValue.red = this.red;
+        nobleValue.blue = this.blue;
+        nobleValue.green = this.green;
+        nobleValue.brown = this.brown;
+        nobleValue.white = this.white;
     }
 
     // Update is called once per frame
