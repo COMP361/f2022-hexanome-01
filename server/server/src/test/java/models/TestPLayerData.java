580d287ed242502a18ac972a7d68649f6f16a22a
Joshua Morency <jsmorency@hotmail.com>
2022-11-27 18:10:41 -0500

    more tests

--- server/server/src/test/java/models/TestPLayerData.java
+++ server/server/src/test/java/models/TestPLayerData.java
@@ -16,8 +16,10 @@ public class TestPLayerData {
 		test.setDiscounts(temp);
 		NobleData[] temp1 = {new NobleData()};
 		CardData[] temp2 = {new CardData()};
+		CardData[] temp3 = {new CardData()};
 		test.setNobles(temp1);
 		test.setInventory(temp2);
+		test.setReserved(temp3);
 		
 		assertEquals(test.getToken(), "12345");
 		assertEquals(test.getUsername(), "Steven");
@@ -27,5 +29,7 @@ public class TestPLayerData {
 			assertEquals(test.getNobles()[i], temp1[i]);
 		for(int i = 0; i < temp2.length;i++)
 			assertEquals(test.getInventory()[i], temp2[i]);
+		for(int i = 0; i < temp3.length;i++)
+			assertEquals(test.getReserved()[i], temp3[i]);
 	}
 }
