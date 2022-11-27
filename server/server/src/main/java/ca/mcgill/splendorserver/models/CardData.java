85e48d77a63704924b844e55d27c9e1134edb3ba
Joshua Morency <jsmorency@hotmail.com>
2022-11-27 16:10:46 -0500

    added server side data classes

--- server/server/src/main/java/ca/mcgill/splendorserver/models/CardData.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/CardData.java
@@ -1,5 +1,86 @@
 package ca.mcgill.splendorserver.models;
 
 public class CardData {
+	private int id;
+    private int points;
+    private char bonus;
+    private int bonusAmount;
 
+    private int red;
+    private int blue;
+    private int green;
+    private int brown;
+    private int white;
+	
+    public int getId() {
+		return id;
+	}
+	
+    public void setId(int id) {
+		this.id = id;
+	}
+	
+    public int getPoints() {
+		return points;
+	}
+	
+    public void setPoints(int points) {
+		this.points = points;
+	}
+	
+    public char getBonus() {
+		return bonus;
+	}
+	
+    public void setBonus(char bonus) {
+		this.bonus = bonus;
+	}
+	
+    public int getBonusAmount() {
+		return bonusAmount;
+	}
+	
+    public void setBonusAmount(int bonusAmount) {
+		this.bonusAmount = bonusAmount;
+	}
+	
+    public int getRed() {
+		return red;
+	}
+	
+    public void setRed(int red) {
+		this.red = red;
+	}
+	
+    public int getBlue() {
+		return blue;
+	}
+	
+    public void setBlue(int blue) {
+		this.blue = blue;
+	}
+	
+    public int getGreen() {
+		return green;
+	}
+	
+    public void setGreen(int green) {
+		this.green = green;
+	}
+	
+    public int getBrown() {
+		return brown;
+	}
+	
+    public void setBrown(int brown) {
+		this.brown = brown;
+	}
+	
+    public int getWhite() {
+		return white;
+	}
+	
+    public void setWhite(int white) {
+		this.white = white;
+	}
 }
--- server/server/src/main/java/ca/mcgill/splendorserver/models/GameData.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/GameData.java
@@ -1,5 +1,31 @@
 package ca.mcgill.splendorserver.models;
 
 public class GameData {
+	private CardData[][] cards;
+	private NobleData[] noblesDisplayed;
+    private PlayerData[] playersInGame; //idk where this data is stored/how to get this data
+    
+	public void setCards(CardData[][] cards) {
+		this.cards = cards;
+	}
 
+	public void setNobles(NobleData[] noblesDisplayed) {
+		this.noblesDisplayed = noblesDisplayed;
+	}
+
+	public void setPlayers(PlayerData[] playersInGame) {
+		this.playersInGame = playersInGame;
+	}
+
+    public CardData[][] getCards() {
+    	return cards;
+    }
+    
+    public NobleData[] getNobles() {
+    	return noblesDisplayed;
+    }
+    
+    public PlayerData[] getPlayers() {
+    	return playersInGame;
+    }
 }
