1c326a877a2307647a7b49ab34020106aa43a6c2
cadinsl <46573438+cadinsl@users.noreply.github.com>
2022-11-28 18:58:45 -0500

    Sample Polling

--- server/server/src/main/java/ca/mcgill/splendorserver/apis/PingController.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/apis/PingController.java
@@ -1,7 +1,8 @@
 package ca.mcgill.splendorserver.apis;
 
 import ca.mcgill.splendorserver.models.SessionData;
-
+import java.util.Date;
+import java.util.Timer;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
+    long startTime = 0L;
+    long elapsedTime = 0L;
+
+
     @GetMapping("/ping")
     public String ping(){
         return "Ping";
+    }
+
+    @GetMapping("/pingPolling")
+    public boolean pingPolling()
+    {
+        if(startTime == 0L)
+        {
+            startTime = System.currentTimeMillis();
+            return false;
+        }
+        elapsedTime = (new Date()).getTime() - startTime;
+        if(elapsedTime > 2*60*1000)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 //    @RequestMapping(value = "/pong", method = RequestMethod.POST, consumes = "application/json")
