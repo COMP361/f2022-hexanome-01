7f19b2b9b15b611915c8299ad54fea76822fbece
Joshua Morency <jsmorency@hotmail.com>
2022-11-28 18:24:31 -0500

    gamedata fix

--- client/Splendor Client/Assets/Scripts/DataManagement/GameData.cs
+++ client/Splendor Client/Assets/Scripts/DataManagement/GameData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class GameData {
     public string gameId;
     public CardData[][] cards;
