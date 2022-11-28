3d381b730c3a2d83f5cf05affc2db05237de92dc
Joshua Morency <jsmorency@hotmail.com>
2022-11-28 17:15:17 -0500

    added more tests

---
+++ server/server/src/test/java/splendorserver/TestGameServiceRegistration.java
@@ -0,0 +1,11 @@
+package splendorserver;
+import static org.junit.Assert.*;
+import org.junit.*;
+import ca.mcgill.splendorserver.*;
+
+public class TestGameServiceRegistration {
+	@Test
+	public void test() {
+		GameServiceRegistrationParameters test = new GameServiceRegistrationParameters("name", "Display", "Location");
+	}
+}
