4f544efa56c2d3bfe84e6afd07f640281d5115f0
kevinjycui <kevinjycui@protonmail.com>
2022-11-28 23:12:12 -0500

    javadoc

--- server/server/src/main/java/ca/mcgill/splendorserver/models/PlayerData.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/PlayerData.java
@@ -56,14 +56,29 @@ public class PlayerData {
     return nobles;
   }
   
+  /**
+   * Sets nobles.
+   *
+   * @param nobles
+   */
   public void setNobles(NobleData[] nobles) {
     this.nobles = nobles;
   }
   
+  /**
+   * Gets discounts.
+   *
+   * @return discounts
+   */
   public int[] getDiscounts() {
     return discounts;
   }
   
+  /**
+   * Sets discounts.
+   *
+   * @param discounts
+   */
   public void setDiscounts(int[] discounts) {
     this.discounts = discounts;
   }
