e86a88f802d83371091d3bcbcb2169057fd2f82d
a-charland <alexandracharland@yahoo.com>
2022-11-28 23:39:54 -0500

    Add javadoc comments to files missing them causing build failure

--- server/server/src/main/java/ca/mcgill/splendorserver/models/NobleData.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/NobleData.java
@@ -11,59 +11,129 @@ public class NobleData {
   private int green;
   private int brown;
   private int white;
-  
+
+  /**
+   * Setter for id.
+   *
+   * @param id int of id
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Setter for points.
+   *
+   * @param points int of points
+   */
   public void setPoints(int points) {
     this.points = points;
   }
 
+  /**
+   * Setter for red.
+   *
+   * @param red int of red
+   */
   public void setRed(int red) {
     this.red = red;
   }
 
+  /**
+   * Setter for blue.
+   *
+   * @param blue int of blue
+   */
   public void setBlue(int blue) {
     this.blue = blue;
   }
 
+  /**
+   * Setter for green.
+   *
+   * @param green in of green
+   */
   public void setGreen(int green) {
     this.green = green;
   }
 
+  /**
+   * Setter for brown.
+   *
+   * @param brown int of brown
+   */
   public void setBrown(int brown) {
     this.brown = brown;
   }
 
+  /**
+   * Setter for white.
+   *
+   * @param white int of white
+   */
   public void setWhite(int white) {
     this.white = white;
   }
 
+  /**
+   * Setter for id.
+   *
+   * @return int of id
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * Getter for points.
+   *
+   * @return int of points
+   */
   public int getPoints() {
     return points;
   }
 
+  /**
+   * Getter for red.
+   *
+   * @return int of red
+   */
   public int getRed() {
     return red;
   }
 
+  /**
+   * Getter for blue.
+   *
+   * @return int of blue
+   */
   public int getBlue() {
     return blue;
   }
 
+  /**
+   * Getter for green.
+   *
+   * @return int of green
+   */
   public int getGreen() {
     return green;
   }
 
+  /**
+   * Getter for brown.
+   *
+   * @return int of brown
+   */
   public int getBrown() {
     return brown;
   }
 
+  /**
+   * Getter for white.
+   *
+   * @return int of white
+   */
   public int getWhite() {
     return white;
   }
--- server/server/src/main/java/ca/mcgill/splendorserver/models/PlayerData.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/PlayerData.java
@@ -11,59 +11,129 @@ public class PlayerData {
   private CardData[] inventory;
   private NobleData[] nobles;
   private int[] discounts = new int[5]; //order: red, green, blue, brown, white
-  
+
+  /**
+   * Getter for username.
+   *
+   * @return username string
+   */
   public String getUsername() {
     return username;
   }
-  
+
+  /**
+   * Setter for username.
+   *
+   * @param username username string
+   */
   public void setUsername(String username) {
     this.username = username;
   }
-  
+
+  /**
+   * Getter for access token.
+   *
+   * @return access token string
+   */
   public String getAccessToken() {
     return accessToken;
   }
 
+  /**
+   * Setter for access token.
+   *
+   * @param accessToken access token string
+   */
   public void setAccessToken(String accessToken) {
     this.accessToken = accessToken;
   }
 
+  /**
+   * Getter for refresh token.
+   *
+   * @return refresh token string
+   */
   public String getRefreshToken() {
     return refreshToken;
   }
 
+  /**
+   * Setter for refresh token.
+   *
+   * @param refreshToken refresh token string
+   */
   public void setRefreshToken(String refreshToken) {
     this.refreshToken = refreshToken;
   }
 
+  /**
+   * Getter for expiry time.
+   *
+   * @return expiry time of access token
+   */
   public String getExpiresIn() {
     return expiresIn;
   }
 
+  /**
+   * Setter for expiry time.
+   *
+   * @param expiresIn expiry time of access token
+   */
   public void setExpiresIn(String expiresIn) {
     this.expiresIn = expiresIn;
   }
 
+  /**
+   * Getter for inventory as card data.
+   *
+   * @return inventory as card data array
+   */
   public CardData[] getInventory() {
     return inventory;
   }
-  
+
+  /**
+   * Setter for inventory as card data.
+   *
+   * @param inventory card data array
+   */
   public void setInventory(CardData[] inventory) {
     this.inventory = inventory;
   }
-  
+
+  /**
+   * Getter for noble data.
+   *
+   * @return data for nobles
+   */
   public NobleData[] getNobles() {
     return nobles;
   }
   
+  /**
+   * Sets nobles.
+   *
+   * @param nobles data for nobles
+   */
   public void setNobles(NobleData[] nobles) {
     this.nobles = nobles;
   }
   
+  /**
+   * Gets discounts.
+   *
+   * @return discounts as array of int
+   */
   public int[] getDiscounts() {
     return discounts;
   }
   
+  /**
+   * Sets discounts.
+   *
+   * @param discounts discounts as array of int
+   */
   public void setDiscounts(int[] discounts) {
     this.discounts = discounts;
   }
