068b5349fd0dd45d9d0cebf69132b6f17a028f73
a-charland <alexandracharland@yahoo.com>
2022-11-28 23:06:32 -0500

    Add javadoc for select files

--- server/server/src/main/java/ca/mcgill/splendorserver/database/DataManager.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/database/DataManager.java
@@ -9,10 +9,21 @@ import java.util.HashMap;
 public class DataManager {
   HashMap<String, SessionData> sessions = new HashMap<String, SessionData>();
 
+  /**
+   * Setter (put) session.
+   *
+   * @param session data for the session to add
+   */
   public void addSession(SessionData session) {
     sessions.put(session.getSessionName(), session);
   }
 
+  /**
+   * Getter (get) session.
+   *
+   * @param sessionName the name of the session to get
+   * @return session data for the session named as above
+   */
   public SessionData getSession(String sessionName) {
     return sessions.get(sessionName);
   }
