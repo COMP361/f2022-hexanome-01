a7fa2e6da7e6186edd9b705037eb7808229a3fa1
a-charland <alexandracharland@yahoo.com>
2022-11-28 23:16:00 -0500

    Merge for adding javadocs to select files

--- server/server/src/test/java/apis/TestGameController.java
+++ server/server/src/test/java/apis/TestGameController.java
@@ -1,21 +1,27 @@
 package apis;
-import static org.junit.Assert.*;
-import org.junit.*;
-import ca.mcgill.splendorserver.apis.*;
-import ca.mcgill.splendorserver.models.*;
 
+import static org.junit.Assert.assertEquals;
 
-//public class TestGameController {
-//	@Test
-//	public void Test() {
-//		GameController test = new GameController();
-//		GameData temp = new GameData();
-//		temp.setGameId("test");
-//
-//		test.createGame(temp);
-//
-//		assertEquals(test.getGame("test"), temp);
-//		assertEquals(test.launchGame(temp.getGameId(), temp), temp.getGameId());
-//		assertEquals(test.getGame((new GameData()).getGameId()), null);
-//	}
-//}
+import ca.mcgill.splendorserver.apis.GameController;
+import ca.mcgill.splendorserver.models.GameData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Test;
+
+/**
+ * game controller test.
+ */
+
+public class TestGameController {
+  @Test
+  public void test() throws JsonProcessingException {
+    GameController test = new GameController();
+    GameData temp = new GameData();
+    temp.setGameId("test");
+
+    test.createGame(temp);
+
+    assertEquals(test.getGame("test"), temp);
+    assertEquals(test.launchGame(temp.getGameId(), temp), temp.getGameId());
+    assertEquals(test.getGame((new GameData()).getGameId()), null);
+  }
+}
