9915de8018320073f31ec6873b6d8bf46be7883d
Joshua Morency <jsmorency@hotmail.com>
2022-12-24 17:24:41 -0500

    removed extraneous card/noble prefab, added comments to (hopefully) all new code, fixed bug with sacrificing that wont let you pick 2 cards, fixed some orient scriptable object having incorrect information

--- client/Splendor Client/Assets/Scripts/Game/Nobles/NobleSlot.cs
+++ client/Splendor Client/Assets/Scripts/Game/Nobles/NobleSlot.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NobleSlot : MonoBehaviour
-{
+public class NobleSlot : MonoBehaviour {
     [SerializeField] private Noble noble;
     [SerializeField] private OrientMenuManager omm;
     [SerializeField] private Image image;
@@ -13,33 +12,31 @@ public class NobleSlot : MonoBehaviour
 
     private bool active = true;
 
-    public void SetupInventory(Noble noble) {
+    public void SetupInventory(Noble noble) { //sets all the values for use in inventory/orient menu
         this.noble = noble;
+        if (!omm) //if not part of orient menu, disable button component
+            gameObject.GetComponent<Button>().interactable = false;
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         m_SpriteRenderer.sprite = noble.sprite;
         image.sprite = noble.sprite;
     }
-    public void SetupOrient(OrientMenuManager omm, Noble noble) {
+    public void SetupOrient(OrientMenuManager omm, Noble noble) { //sets orient specific info + regular inventory info, used when filling orient menu with options
         this.omm = omm;
         image.color = Color.gray;
         SetupInventory(noble);
     }
 
-    public void PassToOrient() {
-        if (omm) {
-            omm.Setup(noble);
-            omm.ResetHighlightedCard();
-        }
+    public void PassToOrient() { //passes noble to orient 
+        omm.Setup(noble);
+        omm.ResetHighlightedCard();
     }
-    public void GreyOut()
-    {
+    public void GreyOut() {
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         m_SpriteRenderer.color = Color.grey;
         active = false;
     }
 
-    public void UnGreyOut()
-    {
+    public void UnGreyOut() {
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         m_SpriteRenderer.color = Color.white;
         active = true;
@@ -47,18 +44,16 @@ public class NobleSlot : MonoBehaviour
     /*Sets the card field to the card received as input and 
     updates the sprite render to display image of card
     */
-    public void SetNoble(Noble noble)
-    {
+    public void SetNoble(Noble noble) { //only used when displaying nobles on board i believe
         this.noble = noble;
         gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
         gameObject.GetComponent<Button>().interactable = false;
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
         m_SpriteRenderer.sprite = noble.sprite;
-        image.color = Color.clear;
+        image.color = Color.clear; //sets canvas image to clear, since this method is used by playerControl and not orientMenuManager (which uses the image for display purposes)
     }
 
-    public Noble GetNoble()
-    {
+    public Noble GetNoble() {
         return noble;
     }
 }
--- client/Splendor Client/Assets/Scripts/MenuManagers/OrientMenuManager.cs
+++ client/Splendor Client/Assets/Scripts/MenuManagers/OrientMenuManager.cs
@@ -13,12 +13,12 @@ public enum ActionType {
     DOMINO2,
 }
 public class OrientMenuManager : MonoBehaviour {
-    public GameObject blankNobleSlot, blankCardSlot, content, backoutButton;
-    public PlayerControl playerControl;
-    public Text actionText, errorText;
-    public Card currentCard1, currentCard2;
-    public Noble currentNoble;
-    public bool sacrificing = false, dominoSatchel = false;
+    [SerializeField] private GameObject blankNobleSlot, blankCardSlot, content, backoutButton; //empty noble/card prefab, content = panel to display options, and button to backout of a sacrifice
+    [SerializeField] private PlayerControl playerControl;
+    [SerializeField] private Text actionText, errorText; //text boxes
+    private Card currentCard1, currentCard2; //selected cards
+    private Noble currentNoble; //selected noble
+    private bool sacrificing = false, dominoSatchel = false; //flags
 
     //note: deck indexs in allcards is: 0 = vanilla1, 1 = vanilla2, 2 = vanilla3, 3 = orient1, 4 = orient2, 5 = orient3
     //note: all 'domino' cards for level 1 also have satchels on them
@@ -30,32 +30,32 @@ public class OrientMenuManager : MonoBehaviour {
     //note: for sacrifices, must discard with priority, cards with a satchel
     //note: satchels occur before every other action.
 
-    public void SacrificingStatus() {
+    public void SacrificingStatus() { //flips sacrifice status (needed for use with a button)
         sacrificing = !sacrificing;
     }
 
-    public void MenuStatus() {
+    public void MenuStatus() { //flips menu status (needed for use with a button)
         playerControl.inOrientMenu = !playerControl.inOrientMenu;
     }
 
-    void ClearChildren() {
+    void ClearChildren() { //clears all things in the menu
         foreach (Transform child in content.transform)
             Destroy(child.gameObject);
     }
 
-    void ResetSelections() {
+    void ResetSelections() {  //reset selections from previous menu usage
         currentNoble = null;
         currentCard1 = null;
         currentCard2 = null;
         errorText.text = "";
     }
 
-    public void ResetHighlightedCard() {
-        foreach (Transform c in content.transform) {
-            if ((c.TryGetComponent(out CardSlot cs) && !(cs.GetCard().Equals(currentCard1) || cs.GetCard().Equals(currentCard2)))
+    public void ResetHighlightedCard() { //highlight/lowlight cards depending on if theyre selected
+        foreach (Transform c in content.transform) { //for all displayed elemenets
+            if ((c.TryGetComponent(out CardSlot cs) && !(cs.GetCard().Equals(currentCard1) || cs.GetCard().Equals(currentCard2))) //if it is a card AND NOT selected, OR if it is a noble AND NOT selected, lowlight it
                 || (c.TryGetComponent(out NobleSlot ns) && !ns.GetNoble().Equals(currentNoble)))
                 c.GetComponent<Image>().color = Color.gray;
-            else
+            else //otherwise, card/noble is selected so highlight it
                 c.GetComponent<Image>().color = Color.white;
         }
     }
@@ -81,19 +81,18 @@ public class OrientMenuManager : MonoBehaviour {
             currentCard1 = card;
     }
 
-    public void Setup(Noble noble) {
+    public void Setup(Noble noble) { //selects/deselects the passed noble
         currentNoble = noble.Equals(currentNoble) ? null : noble;
     }
 
     //methods for making sacrificial cards, given player inventory and currentCardBonus (check images for corresponding bonus-to-sacrifice)
     public void DisplaySacrificialCards() {
         actionText.text = "Please select which card(s) you wish to expend to acquire the new card.";
-        sacrificing = true;
-        char desired = ' ';
-        List<Object> tempAll = new List<Object>();
-        List<Object> tempSatchels = new List<Object>();
+        char desired = '\0';
+        List<Object> tempAll = new List<Object>(); //list for all cards with desired bonus
+        List<Object> tempSatchels = new List<Object>(); //list for all cards with desired bonus AND satchels
         switch (currentCard1.GetBonus()) { //god i wished we used enums for gems. if ordered right, it could be one line of code instead of a switch
-            case 'W': desired = 'K'; break;
+            case 'W': desired = 'K'; break; //sets desired bonus
             case 'R': desired = 'G'; break;
             case 'B': desired = 'W'; break;
             case 'G': desired = 'B'; break;
@@ -106,14 +105,14 @@ public class OrientMenuManager : MonoBehaviour {
             else if (tempSatchels.Count == 0 && c.GetBonus() == desired) //if no satchel cards, add cards with desired discount
                 tempAll.Add(c);
         }
-
-        if (tempSatchels.Count > 0) //if any satchel cards, only display those ones. otherwise, display all aplicable cards
+        //NOTE: could probably do this using a lambda expression to just filter cards out, might be more succinct
+        if (tempSatchels.Count > 0) //if any satchel cards, only display those ones. otherwise, display all applicable cards
             PopulateMenu(tempSatchels);
         else
             PopulateMenu(tempAll);
     }
-    //methods for displaying cards in 2nd exRow for domino2, then does event for next card
 
+    //methods for displaying cards in 2nd exRow for domino2, then does event for next card
     //methods for displaying cards in 1st exRow for domino1, which after new cards is bought then does satchel event for current card, then event for next card
     public void DisplayDominoCards(int dominoLevel) {
         actionText.text = "Please select which card you wish to receive for free.";
@@ -148,50 +147,50 @@ public class OrientMenuManager : MonoBehaviour {
         actionText.text = "Please select which noble you wish to reserve.";
         List<Object> temp = new List<Object>();
         foreach (NobleSlot ns in playerControl.allNobles.nobles)
-            if(ns)
+            if (ns)
                 temp.Add(ns.GetNoble());
         PopulateMenu(temp);
     }
 
-    void PopulateMenu(List<Object> items) {
-        ClearChildren();
-        ResetSelections();
-        foreach (Object i in items) {
-            if(i is Card) {
+    void PopulateMenu(List<Object> items) { //fill the menu with the objects passed in the list
+        ClearChildren(); //first clear menu of all remnants of previous populating
+        ResetSelections(); //reset all card selections since this is a new selection
+        foreach (Object i in items) { //for all objects in the list
+            if (i is Card) { //if the object is a card
                 GameObject temp = Instantiate(blankCardSlot, content.transform.position, Quaternion.identity);
                 temp.transform.SetParent(content.transform);
                 //temp.transform.localScale = new Vector3(1, 1, 1);
                 temp.GetComponent<CardSlot>().SetupOrient(this, i as Card);
             }
-            else {
+            else { //otherwise, object is a noble
                 GameObject temp = Instantiate(blankNobleSlot, content.transform.position, Quaternion.identity);
                 temp.transform.SetParent(content.transform);
                 //temp.transform.localScale = new Vector3(0.2f, 0.4f, 1);
                 temp.GetComponent<NobleSlot>().SetupOrient(this, i as Noble);
             }
-        }   
+        }
     }
     public void PerformAction(Card card) {
-        currentCard1 = card;
-        switch (card.action) {
+        currentCard1 = card; //temporarily set currentCard1 to the action card, only necessary for sacrifices so it knows what discount is needed
+        switch (card.action) { //perform action that card specifies
             case ActionType.RESERVE: DisplayReservableNobles(); break;
             case ActionType.SACRIFICE: SacrificingStatus(); backoutButton.SetActive(true); DisplaySacrificialCards(); break;
             case ActionType.DOMINO2: DisplayDominoCards(2); break;
             case ActionType.DOMINO1: dominoSatchel = true; DisplayInventory(); break; //will need to do satchel part first/as well
             case ActionType.SATCHEL: DisplayInventory(); break;
-            case ActionType.NONE: playerControl.inOrientMenu = false; gameObject.SetActive(false); break;
+            case ActionType.NONE: MenuStatus(); gameObject.SetActive(false); break;
         }
     }
 
     //perform action given selected card(s)
-    public void ConfirmChoice() { //*****confirm that cards for dominos are given to player, and sacrifices are removed*****
-        if (currentNoble) {
+    public void ConfirmChoice() { //confirm button logic
+        if (currentNoble) { //if a noble is selected, reserve it and close menu
             playerControl.ReserveNoble(currentNoble);
             MenuStatus();
             gameObject.SetActive(false);
         }
-        else if (currentCard1) {
-            if (dominoSatchel) { //if current card is the pair for a domino1 satchel
+        else if (currentCard1) { //otehrwise, if at least 1 card is selected
+            if (dominoSatchel) { //if current card is the pair for a domino1 satchel, add satchel to card and do domino action
                 dominoSatchel = false;
                 currentCard1.AddSatchel();
                 playerControl.client.bonusesAquired.ChangeGemAmount(currentCard1.GetBonus(), 1);
@@ -208,23 +207,21 @@ public class OrientMenuManager : MonoBehaviour {
                     SacrificingStatus();
                     gameObject.SetActive(false);
                 }
-                else
+                else //error message for insufficient sacrifice
                     errorText.text = "Error: No enough discounts selected to purchase this card.";
             }
-            else if (playerControl.client.inventory.Contains(currentCard1)) { //if performing satchel action, i.e. of chosen card is in inventory but not sacrificial
+            else if (playerControl.client.inventory.Contains(currentCard1)) { //if performing satchel action, i.e. if chosen card is in inventory but we're not sacrificing
                 currentCard1.AddSatchel();
                 playerControl.client.bonusesAquired.ChangeGemAmount(currentCard1.GetBonus(), 1);
                 MenuStatus();
                 gameObject.SetActive(false);
             }
-            else {
+            else { //otherwise, perform domino actions (i.e. add selected card to inventory for free, and perform corresponding action)
                 playerControl.AcquireCard(currentCard1);
-                PerformAction(currentCard1); //perform action for free domino card
+                PerformAction(currentCard1);
             }
         }
-        else
+        else //error message if nothing is selected
             errorText.text = "Error: No Selection made.";
     }
 }
-
-//TODO: if there are no nobles to reserve, or no cards to select for domino, player still acquires the orient card but does not do associated action
