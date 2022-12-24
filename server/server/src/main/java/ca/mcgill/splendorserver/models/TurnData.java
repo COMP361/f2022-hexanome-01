cf26c37021d0e9f5cbeb3ebc3a48e476cc04a12c
Joshua Morency <jsmorency@hotmail.com>
2022-12-24 14:50:21 -0500

    fixed graphical bugs with satchels, fixed bug with sacrificing cards, everything *should* be consistent between server and client. fixed some bugs with the nobleRow not understanding that nobles had been removed. player now tracks their discounts with inclusion of satchels.

--- server/server/src/main/java/ca/mcgill/splendorserver/models/TurnData.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/TurnData.java
@@ -4,8 +4,8 @@ package ca.mcgill.splendorserver.models;
  * Data passed for each turn.
  */
 public class TurnData {
-  private CardData cardTaken;
-  private NobleData nobleTaken;
+  private CardData[] cardTaken;
+  private NobleData[] nobleTaken;
   
   /**
    * Constructs turn data.
@@ -13,7 +13,7 @@ public class TurnData {
    * @param cardTaken card taken of null if none
    * @param nobleTaken noble taken or null if none
    */
-  public TurnData(CardData cardTaken, NobleData nobleTaken) {
+  public TurnData(CardData[] cardTaken, NobleData[] nobleTaken) {
     this.cardTaken = cardTaken;
     this.nobleTaken = nobleTaken;
   }
@@ -23,7 +23,7 @@ public class TurnData {
    *
    * @return row index
    */
-  public CardData getCardTaken() {
+  public CardData[] getCardTaken() {
     return this.cardTaken;
   }
   
@@ -32,7 +32,7 @@ public class TurnData {
    *
    * @return noble index
    */
-  public NobleData getNobleTaken() {
+  public NobleData[] getNobleTaken() {
     return this.nobleTaken;
   }
 }
