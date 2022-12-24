dc51112edf3f84ece3dbaa3d3f687a16e56fcb87
a-charland <alexandracharland@yahoo.com>
2022-12-24 17:48:11 -0500

    Simplify main menu design

--- client/Splendor Client/Assets/Scripts/MenuManagers/LobbyView/DisplayBar.cs
+++ client/Splendor Client/Assets/Scripts/MenuManagers/LobbyView/DisplayBar.cs
@@ -20,6 +20,6 @@ public class DisplayBar : MonoBehaviour
         else if (fourPlayersToggle.isOn) maxNum = 4;
 
         //int max = int.TryParse(maxNum.text, out int result);dicarded
-        displayBar.text = String.Format("PLAYERS 1/{0}", maxNum);
+        displayBar.text = String.Format("1 player out of {0} players", maxNum);
     }
 }
