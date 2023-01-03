872c3489c175788690a5f3c3babe5a5ec87a9871
a-charland <alexandracharland@yahoo.com>
2023-01-03 11:05:09 -0500

    Simplify inventory panel design and add an inventory panel option for each player

--- client/Splendor Client/Assets/Scripts/Game/MainGame/Dashboard.cs
+++ client/Splendor Client/Assets/Scripts/Game/MainGame/Dashboard.cs
@@ -15,24 +15,30 @@ public class Dashboard : MonoBehaviour
     public Text rTokenDisplay;
     public Text wTokenDisplay;
 
+    public Text bBonusDisplay;
+    public Text gBonusDisplay;
+    public Text kBonusDisplay;
+    public Text rBonusDisplay;
+    public Text wBonusDisplay;
+
     public void UpdatePtsDisplay(int pts)
     {
-        ptsDisplay.text = String.Format("Points: {0}", pts);
+        ptsDisplay.text = String.Format("{0}", pts);
     }
 
     public void DisplayPurchase()
     {
-        endDisplay.text = "Purchase";
+        endDisplay.text = "purchase";
     }
 
     public void DisplayWaiting()
     {
-        endDisplay.text = "Waiting";
+        endDisplay.text = "waiting";
     }
 
     public void ResetEndDisplay()
     {
-        endDisplay.text = "End Turn";
+        endDisplay.text = "end turn";
     }
 
     public void UpdateTokenDisplay(CardGemValue inventory)
@@ -43,4 +49,16 @@ public class Dashboard : MonoBehaviour
         rTokenDisplay.text = String.Format("{0}", inventory.red);
         wTokenDisplay.text = String.Format("{0}", inventory.white);
     }
+
+    /*
+     * Eventually should update the bonus display as well as the token display.
+    public void UpdateBonusDisplay(CardGemValue inventory)
+    {
+        bBonusDisplay.text = String.Format("{0}", inventory.blue);
+        gBonusDisplay.text = String.Format("{0}", inventory.green);
+        kBonusDisplay.text = String.Format("{0}", inventory.brown);
+        rBonusDisplay.text = String.Format("{0}", inventory.red);
+        wBonusDisplay.text = String.Format("{0}", inventory.white);
+    }
+    */
 }
