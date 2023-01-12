aab5b33fe61e45e11c5c6691caeaea2ff2e05ac6
Joshua-M1 <jsmorency@hotmail.com>
2023-01-12 09:10:28 -0500

    did checkstyle, added readme info for libraries just in case

--- server/server/src/main/java/ca/mcgill/splendorserver/apis/JsonHandler.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/apis/JsonHandler.java
@@ -1,16 +1,17 @@
 package ca.mcgill.splendorserver.apis;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class JSONHandler {
+
+/**
+ * JSON Handling class for the server.
+ */
+public class JsonHandler {
   private static JSONParser jsonParser = new JSONParser();
 
   //JSONObject is essentially a hashmap. for encoding, you pass a hashmap 
@@ -28,30 +29,48 @@ public class JSONHandler {
 
   //MAYBE MAKE IT <Object, Object> hashmap instead? to allow mapping something like string => obj
 
+  /**
+   * Encodes a hashmap to a JSONObject.
+   *
+   * @param type - for optional additional info, and hasmap of parameters.
+   * @return JSONObject representation
+   */
   @SuppressWarnings("unchecked")
-  public static JSONObject encodeJsonRequest(String type, HashMap<String, String> params) {
+  public static JSONObject encodeJsonRequest(String type, HashMap<String, String> parameters) {
     JSONObject obj = new JSONObject();
 
     obj.put("type", type);
           
-    if (params != null) {
-      obj.put("params", params);
+    if (parameters != null) {
+      obj.put("parameters", parameters);
     }      
-    return obj; //NOTE: obj.get("type") is type, obj.get("params") is the hasmap of info
+    return obj; //NOTE: obj.get("type") is type, obj.get("parameters") is the hasmap of info
   }
   
+  /**
+   * Encodes a hashmap to a JSONArray.
+   *
+   * @param type for optional additional info, and arraylist of parameters.
+   * @return JSONObject representation
+   */
   @SuppressWarnings("unchecked")
-  public static JSONArray encodeJsonRequest(String type, ArrayList<String> params) {
+  public static JSONArray encodeJsonRequest(String type, ArrayList<String> parameters) {
     JSONArray arr = new JSONArray();
 
     arr.add(type);
           
-    if (params != null) {
-      arr.add(params);
+    if (parameters != null) {
+      arr.add(parameters);
     }      
     return arr; //NOTE: arr.get(0) is type, arr.get(1) is the array of info
   }
-	
+
+  /**
+   * Dencodes a hashmap to a JSON-type field.
+   *
+   * @param message JSON string.
+   * @return object (either a JSONObject, JSONArray, or JSONValue).
+   */
   public static Object decodeJsonRequest(String message) {
     try {
       return jsonParser.parse(message);
@@ -59,14 +78,5 @@ public class JSONHandler {
       e.printStackTrace();
       return null;
     }
-  }
-  
-  public static Object readJsonFile(String filename) {
-    try {
-      return jsonParser.parse(new FileReader(filename));
-    } catch (IOException | ParseException e) {
-      e.printStackTrace();
-    }
-    return null;
   }
 }
