74d1773195e821a0c88a46acc54f1ac709b9628c
Jeremy1999 <jeremyeichler1999@gmail.com>
2023-02-10 13:22:04 -0500

    Iunlockable interface, semi completed player and and inventory class

--- server/server/src/main/java/ca/mcgill/splendorserver/models/Inventory.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/Inventory.java
@@ -1,9 +1,15 @@
 package ca.mcgill.splendorserver.models;
 
+import java.util.ArrayList;
+
 public class Inventory {
 	
 	private TokenBank tokens;
 	private TokenBank bonus;
+	private ArrayList<Card> cards;
+	private ArrayList<Noble> nobles;
+	private ArrayList<Card> reservedCards;
+	private ArrayList<Noble> reservedNobles;
 	
 	public Inventory() {
 		tokens = new TokenBank();
@@ -14,4 +20,27 @@ public class Inventory {
 		return tokens.addAll(acquiredTokens);
 	}
 
+	public void reserve(Card card){
+		reservedCards.add(card);
+	}
+	public void reserveNoble(Noble noble){
+		reservedNobles.add(noble);
+	}
+
+	public void addNobleToInventory(Noble noble){
+		nobles.add(noble);
+	}
+	public void addCard(Card card){
+		cards.add(card);
+	}
+	public boolean removeCard(Card card){
+		return cards.remove(card);
+	}
+	public TokenBank getTokens() {
+		return tokens;
+	}
+
+	public TokenBank getBonus() {
+		return bonus;
+	}
 }
--- server/server/src/main/java/ca/mcgill/splendorserver/models/Player.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/Player.java
@@ -1,7 +1,28 @@
 package ca.mcgill.splendorserver.models;
 
+import java.util.ArrayList;
+
 public class Player {
 	
-	
+	private Inventory inventory;
+	private ArrayList<IUnlockable> unlockables;
+	private Card lastAquired;
+
+	public void aquireCard(Card card){
+	    inventory.addCard(card);
+    }
+    public boolean removeCard(Card card){
+	    return inventory.removeCard(card);
+    }
+
+    public ArrayList<IUnlockable> getUnlockables(){
+        return unlockables;
+    }
+    public Card getLastAcquired(){
+        return lastAquired;
+    }
+
+
+
 
 }
