da0ea784770aca4f99c3e950028681ce574a4cd6
kevinjycui <kevinjycui@protonmail.com>
2023-02-09 22:48:56 -0500

    Add constructor

--- server/server/src/main/java/ca/mcgill/splendorserver/models/Board.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/Board.java
@@ -5,8 +5,8 @@ public class Board {
 	private TokenBank boardTokens;
 	private CardBank cards;
 	
-	public Board() {
-		boardTokens = new TokenBank();
+	public Board(String[] playerList) {
+		boardTokens = new TokenBank(playerList.length + (playerList.length == 4 ? 3:2));
 		cards = new CardBank();
 	}
 	
--- server/server/src/main/java/ca/mcgill/splendorserver/models/Inventory.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/Inventory.java
@@ -5,6 +5,11 @@ public class Inventory {
 	private TokenBank tokens;
 	private TokenBank bonus;
 	
+	public Inventory() {
+		tokens = new TokenBank();
+		bonus = new TokenBank();
+	}
+	
 	public boolean addTokens(String[] acquiredTokens) {
 		return tokens.addAll(acquiredTokens);
 	}
