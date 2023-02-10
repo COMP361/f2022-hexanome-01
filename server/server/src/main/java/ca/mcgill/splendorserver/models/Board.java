49b6afb0f2562ca366564284cd2cb9a20bffdc9b
kevinjycui <kevinjycui@protonmail.com>
2023-02-09 22:45:01 -0500

    Fix more MVC compliance

--- server/server/src/main/java/ca/mcgill/splendorserver/models/Board.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/Board.java
@@ -2,4 +2,12 @@ package ca.mcgill.splendorserver.models;
 
 public class Board {
 	
+	private TokenBank boardTokens;
+	private CardBank cards;
+	
+	public Board() {
+		boardTokens = new TokenBank();
+		cards = new CardBank();
+	}
+	
 }
