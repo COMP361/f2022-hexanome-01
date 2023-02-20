2994ab310222a0a5544dee176d20afc9f6b8a158
YANG TIAN <yang.tian2@mail.mcgill.ca>
2023-02-20 18:24:37 -0500

    added scriptable tokens

---
+++ client/Splendor Client/Assets/Scripts/Game/Gems/TokenSlot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TokenSlot : MonoBehaviour
+{
+    public Gem token;
+    public Text amount;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initialises the displayed amount text to amount given to scriptableobject
+        amount.text = token.amount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
