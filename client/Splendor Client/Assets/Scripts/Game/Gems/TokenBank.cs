71a72eb4cf1ce6ac70761c3bafc897ec4a38778b
YANG TIAN <yang.tian2@mail.mcgill.ca>
2023-02-20 20:29:37 -0500

    fixed null reference and added tokenBank

--- client/Splendor Client/Assets/Scripts/Game/Gems/TokenBank.cs
+++ client/Splendor Client/Assets/Scripts/Game/Gems/TokenBank.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TokenBank : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TokenBank : MonoBehaviour{
+    public List<Gem> tokens = new List<Gem>();
+    public int gold;
+    public int blue;
+    public int green;
+    public int black;
+    public int red;
+    public int white;
+
+    public void set(int n){
+        gold = 5;
+        blue = n;
+        green = n;
+        black = n;
+        red = n;
+        white = n;
+        foreach (Gem gem in tokens){
+            if (gem.colour != "gold"){
+                gem.amount = n;
+            }
+            else{gem.amount = 5;}
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //hard coded token amount; the real amount should be different according to number of players
+    //gold token is always 5 regardless of number of players
+    void Start(){
+        foreach (Gem gem in tokens){
+            if (gem.colour != "gold"){
+                gem.amount = 7;
+            }
+            else{gem.amount = 5;}
+        }
+        gold = 5;
+        blue = 7;
+        green = 7;
+        black = 7;
+        red = 7;
+        white = 7;
     }
 }
