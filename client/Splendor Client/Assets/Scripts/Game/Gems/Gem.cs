8eb65195b9eeb0d1a08615f69d111ecfd2bed7b3
YANG TIAN <yang.tian2@mail.mcgill.ca>
2023-02-23 00:10:02 -0500

    still cannot fix raycast for tokens

--- client/Splendor Client/Assets/Scripts/Game/Gems/Gem.cs
+++ client/Splendor Client/Assets/Scripts/Game/Gems/Gem.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Events;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 // Gem is the same thing as token (the in game currency)
 // The name "gem" is to differentiate the "token" from the one used for users
@@ -10,6 +14,8 @@ public class Gem : ScriptableObject{
     [SerializeField] public int amount;
     [SerializeField] public string colour;
     public Sprite sprite;
+    private SpriteRenderer m_SpriteRenderer;
+    [SerializeField] private Image image;
 
     public void setAmount(int n){
         amount = n;
--- client/Splendor Client/Assets/Scripts/Game/Gems/TokenSlot.cs
+++ client/Splendor Client/Assets/Scripts/Game/Gems/TokenSlot.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using UnityEngine.Events;
 
 public class TokenSlot : MonoBehaviour
 {
     public Gem token;
     public Text amount;
     public Image image;
+    [SerializeField] private SelectedTokens selectedTokens;
+
+    public Gem get(){
+        return token;
+    }
 
     // Start is called before the first frame update
     void Start()
