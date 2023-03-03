75b0bd5ead1f092f2edb7ccd117e648b3d2431d0
a-charland <alexandracharland@yahoo.com>
2023-03-03 17:23:26 -0500

    Update taking tokens UI to avoid expansions overlap

--- client/Splendor Client/Assets/Scripts/EndTurnUI.cs
+++ client/Splendor Client/Assets/Scripts/EndTurnUI.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndTurnUI : MonoBehaviour
 {
+    public Text buttonText;
     public PlayerControl playerControl;
 
     void OnClick()
