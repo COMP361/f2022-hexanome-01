58cfa0d0c244714936d8892b8091ae8706026ccb
kevinjycui <kevinjycui@protonmail.com>
2023-03-06 17:38:31 -0500

    Error text script

---
+++ client/Splendor Client/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{
+
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        CompleteFade();
+    }
+
+    public void ResetFade()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+    }
+
+    public void CompleteFade()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (text.color.a > 0.0f)
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / 5f));
+    }
+}
