af1ffd1203dcfcc728159d223f9c679d4b9f245f
YANG TIAN <yang.tian2@mail.mcgill.ca>
2023-03-06 20:41:53 -0500

    scriptable for winning scene

---
+++ client/Splendor Client/Assets/Scripts/Game/WinnerScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinnerScript : MonoBehaviour
+{
+    public Winner winner;
+    public Text message;
+}
