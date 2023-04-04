ef74921703d93167cd6c27be922478c0594b2392
kevinjycui <kevinjycui@protonmail.com>
2023-04-04 16:33:22 -0400

    Admin UI

---
+++ client/Splendor Client/Assets/Scripts/MenuManagers/Back.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Back : MonoBehaviour
+{
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene(1); //loads menu scene
+    }
+}
