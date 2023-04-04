50d05187220ec86a6a1e8d4129736e359a4d18b1
kevinjycui <kevinjycui@protonmail.com>
2023-04-04 17:27:05 -0400

    Admin UI

--- client/Splendor Client/Assets/Scripts/MenuManagers/MainMenuNavigator.cs
+++ client/Splendor Client/Assets/Scripts/MenuManagers/MainMenuNavigator.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Back : MonoBehaviour
+public class MainMenuNavigator : MonoBehaviour
 {
 
     public void MenuScene()
     {
         SceneManager.LoadScene(1); //loads menu scene
+    }
+
+    public void AdminScene()
+    {
+        SceneManager.LoadScene(4); //loads menu scene
     }
 }
