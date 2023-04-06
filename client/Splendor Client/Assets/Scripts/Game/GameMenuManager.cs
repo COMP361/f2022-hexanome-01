a4ad6003a2cef2f7f8a0879f07d6a8f5b00fa2b7
kevinjycui <kevinjycui@protonmail.com>
2023-04-05 23:38:58 -0400

    Load login on exit

--- client/Splendor Client/Assets/Scripts/Game/GameMenuManager.cs
+++ client/Splendor Client/Assets/Scripts/Game/GameMenuManager.cs
@@ -41,15 +41,15 @@ public class GameMenuManager : MonoBehaviour
         yield return StartCoroutine(GameRequestManager.SaveGameServer(currentSession.id, successText, failText));
         Debug.Log("saved game");
         yield return StartCoroutine(GameRequestManager.DeleteGameServer(currentSession.id));
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 
     IEnumerator saveAndExitEndSessionRoutine(){
         yield return StartCoroutine(GameRequestManager.SaveGameServer(currentSession.id, successText, failText));
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
     IEnumerator exitRoutine(){
         yield return StartCoroutine(GameRequestManager.DeleteGameServer(currentSession.id));
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 }
