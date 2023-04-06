04d7974c575c95c3d51449ee0192504944714063
Joshua Morency <jsmorency@hotmail.com>
2023-04-05 21:53:58 -0400

    fix for beaord

--- server/server/src/main/java/ca/mcgill/splendorserver/models/Inventory.java
+++ server/server/src/main/java/ca/mcgill/splendorserver/models/Inventory.java
@@ -359,7 +359,7 @@ public class Inventory implements Serializable {
       satchelCardsJson.add(card.getId());
       satchelCardsJson.add(card.getSatchelCount());
     }
-    json.put("satcheledCards", cardsJson);
+    json.put("satcheledCards", satchelCardsJson);
     
     //nobles
     JSONArray noblesJson = new JSONArray();
